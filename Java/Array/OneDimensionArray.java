4a7619049911ccd9da8e433ee990c6dad580a195
joshbam-joshwilly <j.alana@alustudent.com>
2024-01-10 15:07:09 +0100

    task done for one-dimensional array

---
+++ Java/Array/OneDimensionArray.java
@@ -0,0 +1,18 @@
+public class OneDimensionArray {
+    public static void main(String[] args) {
+        // declaration of array
+        int array[] = new int[5];
+        // initialization of array
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+        array[3] = 4;
+
+        // printing of array
+        for( int i = 0; i<array.length; i++ ) {
+            System.out.println("Array  " + i );
+        }
+
+
+    }
+}
