1fa967a08d914713e1b98f03127750a36cd3d404
joshbam-joshwilly <j.alana@alustudent.com>
2024-01-12 05:58:59 +0100

    static methods

--- PYTHON/static/static.py
+++ PYTHON/static/static.py
@@ -1,14 +1,35 @@
-class Sum:
+# class Sum:
+
+#     @staticmethod
+#     def getSum(*args):
+        
+#         sum = 0
+
+#         for i in args:
+#             sum += i
+#         return sum
+
+# def main():
+#     print(" Sum : ", Sum.getSum(1,2,3,4,5,5,6,))
+# main()
+
+class Dog:
+
+    num_of_nums = 0
+
+    def __init__(self, name="Uknown"):
+        self.name = name
+
+        Dog.num_of_nums += 1
 
     @staticmethod
-    def getSum(*args):
-        
-        sum = 0
-
-        for i in args:
-            sum += i
-        return sum
-
+    def getNumOfDogs():
+        print("There are currently {} dogs".format(Dog.num_of_nums))
 def main():
-    print("Sum : ", Sum.getSum(1,2,3,4,5,5,6,))
+
+    spot = Dog("Spot")
+
+    doug = Dog("Doug")
+
+    spot.getNumOfDogs()
 main()
