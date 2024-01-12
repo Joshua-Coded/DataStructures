2e02af9897cb5495bcb140870114e1ff285f60cb
joshbam-joshwilly <j.alana@alustudent.com>
2024-01-12 10:47:05 +0100

    filter method

--- PYTHON/map/map.py
+++ PYTHON/map/map.py
@@ -4,3 +4,10 @@ def dbl_num(num):
     return num * 2
 
 print(list(map(dbl_num, oneTo10)))
+
+print(list(map((lambda x: x * 3), oneTo10)))
+
+aList = list(map((lambda x, y : x + y), [1, 2, 3], [2, 4, 7]))
+print(aList)
+
+print(list(filter((lambda x: x % 2 == 0), range(1, 11))))
