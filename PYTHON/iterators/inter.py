c1fa4ba0881d44d7f26cbe9e8df981bfef44d2e5
joshbam-joshwilly <j.alana@alustudent.com>
2024-01-16 10:47:41 +0100

    custom inter

--- PYTHON/iterators/inter.py
+++ PYTHON/iterators/inter.py
@@ -1,6 +1,29 @@
-sampStr = iter("Samples")
+# sampStr = iter("Samples")
 
-print("Char : ", next(sampStr))
-print("Char : ", next(sampStr))
-print("Char : ", next(sampStr))
-print("Char : ", next(sampStr))
+# print("Char : ", next(sampStr))
+# print("Char : ", next(sampStr))
+# print("Char : ", next(sampStr))
+# print("Char : ", next(sampStr))
+
+class Alphabet:
+    def __init__(self):
+        self.letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        self.index = -1
+
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.index >= len(self.letters) - 1:
+            raise StopIteration
+        self.index = self.index + 1
+        return self.letters[self.index]
+    
+
+alpa = Alphabet()
+
+for letter in alpa:
+    print(letter)
+
+
+    
