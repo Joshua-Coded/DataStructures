6fa0768ad1e04530e16b8dc7a9e16145d374b075
joshbam-joshwilly <j.alana@alustudent.com>
2024-01-19 11:23:00 +0100

    task

---
+++ PYTHON/regex/problem_2.py
@@ -0,0 +1,16 @@
+import re
+
+# # randStr = "cat cats doctors doctor's"
+
+# randStr = "doctor doctors doctor's"
+
+# # regex = re.compile("[cat]+s?")
+
+# regex = re.compile("[doctor]+['s]*")
+
+# matches = re.findall(regex, randStr)
+
+# for i in matches:
+#     print(i)
+
+### problems 
