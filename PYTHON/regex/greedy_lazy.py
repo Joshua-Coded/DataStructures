fa60201e1b95effd51b68c54962c0c56fdc6636e
joshbam-joshwilly <j.alana@alustudent.com>
2024-01-19 11:38:17 +0100

    multi line

--- PYTHON/regex/greedy_lazy.py
+++ PYTHON/regex/greedy_lazy.py
@@ -1,8 +1,29 @@
 import re
 
-randStr = "<name>Life on mars</name><name>Freaks and geek</name>"
+# randStr = "<name>Life on mars</name><name>Freaks and geek</name>"
 
-regex = re.compile("<name>.*?</name>")
+# regex = re.compile("<name>.*?</name>")
+
+# randStr = "ape at the apex"
+
+# regex = re.compile(r"\bape\b")
+
+# randStr = "match everything up to the @"
+
+# regex = re.compile(r"^.*[^@]")
+
+
+# randStr = "@ get this string"
+
+# regex = re.compile(r"[^@\s].*$")
+
+
+randStr = '''Ape is big
+Turtle is slow
+cheetah is fast
+'''
+
+regex = re.compile(r"(?m)^.*?\s")
 
 match = re.findall(regex, randStr)
 
